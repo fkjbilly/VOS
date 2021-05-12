c30b9558108d4648d5b58df2f9396ac6dedff8ce
PHR <2908035332@qq.com>
2021-05-12 16:13:58 +0800

    替换原img.js，新增css

--- VOS/wwwroot/Img.js
+++ VOS/wwwroot/Img.js
@@ -1,4 +1,23 @@
 ﻿/**
  * 图片显示
- * 图片src  src
+ * @param {any} img
  */
+function ImageAuto(src) {
+    let _src;
+    if (src.indexOf("base64") < 0) {
+        _src = src.replace(/64/g, "640");
+    } else {
+        _src = src;
+    }
+    var imgHtml = "<img src='" + _src + "' width='640px' height='640px'/>";
+    parent.layer.open({
+        type: 1
+        , title: '预览'
+        , area: ["640px", "640px"]
+        , id: "img"
+        , shade: 0.6
+        , maxmin: true
+        , anim: 1
+        , content: imgHtml
+    });
+}
